6cda0e0a996595d30a854b7d617c6bcfff646e19
lily <lily.you@student.tdsb.on.ca>
2025-03-05 18:48:56 +0000

    .

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,33 @@
+import java.io.*; 
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static String readFile(String filename){
+		FileReader inputStream = null;
+		try{
+			inputStream = new FileReader(filename);
+			String result = "";
+			while((c = inputStream.read()) != -1){
+				result += c;
+			}
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		finally{
+			try{
+				if(inputStream != null){
+					inputStream.close();
+				}
+			}
+			catch(IOException e){
+				System.out.println(e);
+			}
+		}
+	}
 
 }
